30c27069ead984033b63a1401dd5fec8a9776650
Yarik Brouwer <i362569@fhict.nl>
2021-11-23 11:59:32 +0100

    Added new project for User
    Made better user, so i can add users when they log in with Microsoft login.

---
+++ User Back-End/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User_Back_End.Logic;
+using User_Back_End.Models;
+using User_Back_End.ViewModels;
+
+namespace User_Back_End.Controllers
+{
+    [Route("users")]
+    [ApiController]
+
+    public class UserController : ControllerBase
+    {
+        private readonly UserLogic _userLogic;
+        public UserController(UserLogic userlogic)
+        {
+            _userLogic = userlogic;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<ActionResult<User>> Login([FromBody] User user)
+        {
+            user = _userLogic.GetUser(user);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return StatusCode(404, "User doesn't exist");
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+        {
+            if (user.Username != null && user.Email != null && user.Address != null && user.City != null)
+            {
+                _userLogic.NewUser(user);
+                return CreatedAtAction("CreateUser", user);
+            }
+            return StatusCode(404, "Not all fields are filled in");
+        }
+
+    }
+}
---
+++ User Back-End/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Back_End.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public Guid ID { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public User()
+        {
+
+        }
+        public User(Guid id, string email, string username, string address, string city)
+        {
+            ID = id;
+            Email = email;
+            Username = username;
+            Address = address;
+            City = city;
+        }
+        public User(string email)
+        {
+            Email = email;
+        }
+    }
+}
